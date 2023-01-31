7254e0dbcfb33c470dc00cfe74f06dd2ce8cd8c3
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-01-31 23:29:40 +0700

    Add input username

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// let modalBtns = [...document.querySelectorAll(".button")];
+// modalBtns.forEach(function (btn) {
+//   btn.onclick = function () {
+//     let modal = btn.getAttribute("data-modal");
+//     document.getElementById(modal).style.display = "block";
+//   };
+// });
+
+// window.onclick = function (event) {
+//   if (event.target.className === "modal") {
+//     event.target.style.display = "none";
+//   }
+// };
