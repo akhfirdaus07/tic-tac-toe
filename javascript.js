7cd9468edf5780a10e0b43934c7d2b19de5e9554
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-02-01 08:15:36 +0700

    Add eventlistener add value to game board when clicked

--- javascript.js
+++ javascript.js
@@ -14,3 +14,14 @@ function addUsername(event) {
     render.username();
     document.getElementById("modalOne").style.display = 'none';
 }
+
+const items=document.querySelectorAll(".item")
+
+function addValueToBoard(){
+    this.textContent="X";
+    this.removeEventListener("click",addValueToBoard)
+}
+
+for(let item of items){
+    item.addEventListener("click", addValueToBoard)
+}
