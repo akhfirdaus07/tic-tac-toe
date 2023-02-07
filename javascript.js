beb6938aba36332584974377775460151d7c1966
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-02-07 22:47:37 +0700

    Add aiMove function

--- javascript.js
+++ javascript.js
@@ -6,6 +6,7 @@ const modalOne = document.getElementById("modalOne");
 const modalTwo = document.getElementById("modalTwo");
 const items=document.querySelectorAll(".item");
 let itemsArr=[];
+let bestMove;
 const humanPlayer = "X";
 const aiPlayer = "O";
 const winningConditions = [
@@ -40,6 +41,7 @@ window.addEventListener("click", function (event) {
     if (event.target.className === "output") {
         modalTwo.style.display = "none";
         render.resetScreen();
+        refreshArrValue();
     }
 });
 addBtn.addEventListener('click', addUsername);
@@ -56,86 +58,155 @@ function addUsername(event) {
 function addValueToBoard(){
     this.textContent="X";
     this.removeEventListener("click",addValueToBoard);
-    //Check for wins and tie
+    refreshArrValue();
+    // //Check for wins and tie
+    
     if(checkHumanWin()) {
         result.innerText = 'You Win!'; 
-        modalTwo.style.display = 'block';   
-    // }else if(checkAiWin()) {
-    //     result.innerText = 'You Lose!'; 
-    //     modalTwo.style.display = 'block';   
-    }else if(isTie()) {
+        modalTwo.style.display = 'block';
+        return;
+    } else if(isTie()) {
         result.innerText = 'Draw!'; 
-        modalTwo.style.display = 'block';   
+        modalTwo.style.display = 'block';
+        return;   
     }
+
+    aiMove();
+    if(checkAiWin()) {
+        result.innerText = 'You Lose!'; 
+        modalTwo.style.display = 'block';  
+        return; 
+    } else if(isTie()) {
+        result.innerText = 'Draw!'; 
+        modalTwo.style.display = 'block';  
+        return;
+    }
+    // if (!checkHumanWin() && !isTie()) turn(bestSpot());
+};
+
+// function humanBestMove(){
+//     // return winningConditions.some((combination) => {
+//     //     return combination.every((i) => {
+//     //       return items[i].innerText === humanPlayer;
+//     //     });
+//     // });
+
+//     for(let winningCondition of winningConditions){
+//         if(winningCondition.filter(word => word.includes(humanPlayer))==2){
+//             bestMove=winningCondition.indexOf("");
+//             return bestMove;
+//         }
+//     }
+// }
+
+
+function aiMove(){
+    
+
+    for(let winningCondition of winningConditions){
+        let count=0;
+    // [0, 1, 2]
+    
+        for(let value of winningCondition){
+            if(itemsArr[value]==humanPlayer) count++;
+        }
+
+        if(count>1){
+            for(let item of winningCondition){
+                if (itemsArr[item]=="") bestMove=item;
+            }
+
+            // console.log(winningCondition)
+            // console.log(winningCondition.indexOf(""))
+            // bestMove=bestMove.indexOf("");
+            
+            break;
+        }
+    }
+    console.log(bestMove)
+    if(bestMove){
+        document.getElementById(bestMove).innerText=aiPlayer;
+        document.getElementById(bestMove).removeEventListener("click",addValueToBoard);
+        bestMove=undefined;
+    }else{
+        for(let i =0; i<itemsArr.length;i++){
+            if(itemsArr[i]==""){
+                document.getElementById(i).innerText=aiPlayer;
+                document.getElementById(i).removeEventListener("click",addValueToBoard);
+                break;
+            }
+        };
+    };
     refreshArrValue();
-    if (!checkHumanWin() && !isTie()) turn(bestSpot(), aiPlayer);
-}
-function turn(squareId, player) {
-    itemsArr[squareId] = player;
-    document.getElementById(squareId).innerText = player;
-    let gameWon = checkWin(origBoard, player)
-    if (gameWon) gameOver(gameWon)
-}
-function bestSpot() {
-    return minimax(itemsArr, aiPlayer).index;
-}
+};
 
-function minimax(newBoard, player) {
-    var availSpots = itemsArr.filter(i => i != "O" && i != "X");
 
-    if (checkHumanWin) {
-        return { score: -10 };
-    } else if (checkAiWin) {
-        return { score: 10 };
-    } else if (availSpots.length === 0) {
-        return { score: 0 };
-    }
-    var moves = [];
-    for (let i = 0; i < availSpots.length; i++) {
-        var move = {};
-        move.index = newBoard[availSpots[i]];
-        newBoard[availSpots[i]] = player;
+// function turn(squareId) {
+//     itemsArr[squareId] = aiPlayer;
+//     document.getElementById(squareId).innerText = aiPlayer;
+//     if(checkHumanWin()) {
+//         result.innerText = 'You Win!'; 
+//         modalTwo.style.display = 'block';    
+//     }
+// }
+// function bestSpot() {
+//     return minimax(itemsArr, aiPlayer).index;
+// }
 
-        if (player == aiPlayer) {
-            let result = minimax(newBoard, huPlayer);
-            move.score = result.score;
-        } else {
-            let result = minimax(newBoard, aiPlayer);
-            move.score = result.score;
-        }
+// function minimax(newBoard, player) {
+//     var availSpots = itemsArr.filter(i => i != "O" && i != "X");
 
-        newBoard[availSpots[i]] = move.index;
+//     if (checkHumanWin()) {
+//         return { score: -10 };
+//     } else if (checkAiWin()) {
+//         return { score: 10 };
+//     } else if (availSpots.length === 0) {
+//         return { score: 0 };
+//     }
+//     var moves = [];
+//     for (let i = 0; i < availSpots.length; i++) {
+//         var move = {};
+//         move.index = newBoard[availSpots[i]];
+//         newBoard[availSpots[i]] = player;
 
-        moves.push(move);
-    }
+//         if (player == aiPlayer) {
+//             let output = minimax(newBoard, humanPlayer);
+//             move.score = output.score;
+//         } else {
+//             let output = minimax(newBoard, aiPlayer);
+//             move.score = output.score;
+//         }
 
-    var bestMove;
-    if (player === aiPlayer) {
-        var bestScore = -10000;
-        for (var i = 0; i < moves.length; i++) {
-            if (moves[i].score > bestScore) {
-                bestScore = moves[i].score;
-                bestMove = i;
-            }
-        }
-    } else {
-        var bestScore = 10000;
-        for (var i = 0; i < moves.length; i++) {
-            if (moves[i].score < bestScore) {
-                bestScore = moves[i].score;
-                bestMove = i;
-            }
-        }
-    }
+//         newBoard[availSpots[i]] = move.index;
 
-    return moves[bestMove];
-}
+//         moves.push(move);
+//     }
+
+//     var bestMove;
+//     if (player === aiPlayer) {
+//         var bestScore = -10000;
+//         for (let i = 0; i < moves.length; i++) {
+//             if (moves[i].score > bestScore) {
+//                 bestScore = moves[i].score;
+//                 bestMove = i;
+//             }
+//         }
+//     } else {
+//         var bestScore = 10000;
+//         for (let i = 0; i < moves.length; i++) {
+//             if (moves[i].score < bestScore) {
+//                 bestScore = moves[i].score; 
+//                 bestMove = i;
+//             }
+//         }
+//     }
+
+//     return moves[bestMove];
+// }
 
 
 
 // Tic-Tac-Toe Logic
-
-
 function checkHumanWin() {
     return winningConditions.some((combination) => {
       return combination.every((i) => {
@@ -151,15 +222,9 @@ function checkAiWin() {
     });
 };
 
-
-
-
 function isTie() {
-    return itemsArr.every((item) => {
-        return item.innerText === "X" || item.innerText === "O";
-    });
-};
-
+    return itemsArr.every(el => el == humanPlayer || el == aiPlayer)
+}; 
 
 function refreshArrValue(){
     itemsArr=[];
